e045baa3a6db081f1438da198ae1d617b1482010
michael <michael@NBDV05.perschmann.local>
2016-10-12 11:24:53 +0200

    Füllen der Testdaten

--- Crossword/test/de/mic/crossword/RaetselTestdatenLoesungen.java
+++ Crossword/test/de/mic/crossword/RaetselTestdatenLoesungen.java
@@ -1,11 +1,32 @@
 package de.mic.crossword;
 
+import static de.mic.crossword.Testfragen.AN_JEDEN_PLATZ_ORT;
+import static de.mic.crossword.Testfragen.AUSFUEHREN_VERRICHTEN;
+import static de.mic.crossword.Testfragen.BALDRIANGEWAECHS;
+import static de.mic.crossword.Testfragen.BIBL_STAMMVATER_ARCHE;
+import static de.mic.crossword.Testfragen.BUNDESSTAAT_DER_USA;
+import static de.mic.crossword.Testfragen.DIE_ACKERKRUME_LOCKERN;
+import static de.mic.crossword.Testfragen.EIWEISSHALTIGE_NUTZPFLANZE;
+import static de.mic.crossword.Testfragen.FRANZ_WEIBLICHER_ARTIKEL;
+import static de.mic.crossword.Testfragen.FREUNDLICH;
+import static de.mic.crossword.Testfragen.GARTENGERAET_HARKE;
+import static de.mic.crossword.Testfragen.HANDARBEITSTECHNIK;
+import static de.mic.crossword.Testfragen.LAUTSTAERKEMASS;
+import static de.mic.crossword.Testfragen.PAPSTKRONE;
+import static de.mic.crossword.Testfragen.REGE_MUNTER;
+import static de.mic.crossword.Testfragen.RUHMESBAU_BEI_REGENSBURG;
+import static de.mic.crossword.Testfragen.WEIBLICHES_BUEHNENFACH;
+import static de.mic.crossword.Testfragen.WESTAFRIKANER;
+import static de.mic.crossword.Testfragen.WIDERHALL;
+import static de.mic.crossword.Testfragen.WUERDIGEN_AUSZEICHNEN_UGS;
+
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 /**
- * Diese Klasse beinhaltet die Lösungen für die Testdaten.
+ * Diese Klasse beinhaltet die Lï¿½sungen fï¿½r die Testdaten.
  * 
  * @author michael
  *
@@ -22,11 +43,36 @@ public class RaetselTestdatenLoesungen {
 	}
 
 	private void fillDaten() {
+		loesungen.put(Testfragen.AN_JEDEN_PLATZ_ORT, Arrays.asList("abc", "def"));
+
+		loesungen.put(DIE_ACKERKRUME_LOCKERN, Arrays.asList("abc", "def"));
+		loesungen.put(WESTAFRIKANER, Arrays.asList("abc", "def"));
+		loesungen.put(GARTENGERAET_HARKE, Arrays.asList("abc", "def"));
+		loesungen.put(BIBL_STAMMVATER_ARCHE, Arrays.asList("abc", "def"));
+		loesungen.put(WUERDIGEN_AUSZEICHNEN_UGS, Arrays.asList("abc", "def"));
+		loesungen.put(EIWEISSHALTIGE_NUTZPFLANZE, Arrays.asList("abc", "def"));
+		loesungen.put(PAPSTKRONE, Arrays.asList("abc", "def"));
+		loesungen.put(LAUTSTAERKEMASS, Arrays.asList("abc", "def"));
+		loesungen.put(BALDRIANGEWAECHS, Arrays.asList("abc", "def"));
+
+		loesungen.put(AN_JEDEN_PLATZ_ORT, Arrays.asList("abc", "def"));
+
+		loesungen.put(RUHMESBAU_BEI_REGENSBURG, Arrays.asList("abc", "def"));
+		loesungen.put(FRANZ_WEIBLICHER_ARTIKEL, Arrays.asList("abc", "def"));
+		loesungen.put(REGE_MUNTER, Arrays.asList("abc", "def"));
+		loesungen.put(BUNDESSTAAT_DER_USA, Arrays.asList("abc", "def"));
+
+		loesungen.put(AUSFUEHREN_VERRICHTEN, Arrays.asList("abc", "def"));
+		loesungen.put(WIDERHALL, Arrays.asList("abc", "def"));
+
+		loesungen.put(HANDARBEITSTECHNIK, Arrays.asList("abc", "def"));
+		loesungen.put(FREUNDLICH, Arrays.asList("abc", "def"));
+		loesungen.put(WEIBLICHES_BUEHNENFACH, Arrays.asList("abc", "def"));
 
 	}
 
 	/**
-	 * Liefert die Testdaten zurück.
+	 * Liefert die Testdaten zurï¿½ck.
 	 * 
 	 * @param frage
 	 * @return
